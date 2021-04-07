fd8395c558dabc081b0d47e61c93744a06a583e3
github username <github email address>
2021-04-07 00:50:12 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // forms and inputs set variables 
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+  // attach the event listener 
+  newTaskForm.addEventListener("submit", createNewTask);
+  // prevent form from submission
+  // reset event 
+  function createNewTask(event) {
+    console.log (event)
+    event.preventDefault();
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+    taskList.appendChild(newTask);
+    event.target.reset();
+  };
 });
